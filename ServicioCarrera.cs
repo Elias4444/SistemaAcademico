c3c55a242c236e6b11cdd173fdf35415a61c0610
Elias4444 <ortizeliasj@gmail.com>
2025-06-13 20:40:38 -0300

    Clase11.1:Eliminar

---
+++ ServicioCarrera.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.Json;
+using SistemaAcademico_V2.Models;
+
+namespace SistemaAcademico_V2.Servicios
+{
+    public class ServicioCarrera
+    {
+        private static string ruta = "Data/carreras.json";
+        public static string LeerTextoDelArchivo()
+        {
+            if (File.Exists(ruta))
+            {
+                return File.ReadAllText(ruta);
+            }
+            return "[]";
+        }
+        public static List<Carrera> ObtenerCarreras()
+        {
+            string json = LeerTextoDelArchivo();
+            var lista = JsonSerializer.Deserialize<List<Carrera>>(json);
+            return lista ?? new List<Carrera>();
+        }
+        public static void AgregarCarrera(Carrera nuevaCarrera)
+        {
+            var carreras = ObtenerCarreras();
+            nuevaCarrera.Id = ObtenerNuevoId(carreras);
+            carreras.Add(nuevaCarrera);
+            GuardarCarreras(carreras);
+        }
+        public static int ObtenerNuevoId(List<Carrera> carreras)
+        {
+            int maxId = 0;
+            foreach (var carrera in carreras)
+            {
+                if (carrera.Id == maxId)
+                {
+                    maxId = carrera.Id;
+                }
+            }
+            return maxId + 1;
+        }
+        public static void GuardarCarreras(List<Carrera> carreras)
+        {
+            string textoJson = JsonSerializer.Serialize(carreras);
+            File.WriteAllText(ruta, textoJson);
+        }
+        public static Carrera BuscarPorId(int id)
+        {
+            var lista = ObtenerCarreras();
+            foreach (var carrera in lista)
+            {
+                if (carrera.Id == id)
+                {
+                    return carrera;
+                }
+            }
+            return null;
+        }
+        public static void EliminarPorId(int id)
+        {
+            var lista = ObtenerCarreras();
+            Carrera? carreraAEliminar = null;
+            foreach (var carrera in lista)
+            {
+                if (carrera.Id == id)
+                {
+                    carreraAEliminar = carrera;
+                    break;
+                }
+            }
+            if (carreraAEliminar != null)
+            {
+                lista.Remove(carreraAEliminar);
+                GuardarCarreras(lista);
+            }
+        }
+    }
+}
